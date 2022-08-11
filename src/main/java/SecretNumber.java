9021337b89e32d0e291346807e3bacd1c91b5301
Faizan Khan <faizanahmkhan@gmail.com>
2022-08-11 23:35:54 +0100

    created secret number guessing game

--- src/main/java/SecretNumber.java
+++ src/main/java/SecretNumber.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class SecretNumber {
 
     public static void main(String[] args) {
-        
+//        Prompt user to guess a number
+        System.out.println("Are you feeling lucky? Take a chance guessing the secret number...");
+
+//        Collect user input
+        Scanner reader = new Scanner(System.in);
+        String secretNumber = reader.nextLine();
+
+//        if chocolate is 3, print 'correct'
+//        otherwise print 'incorrect'
+
+        if (secretNumber.equals("3")){
+            System.out.println("That's correct!!");
+        } else {
+            System.out.println("Wrong! Now leave.");
+        }
+
     }
 
 }
--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -1,2 +1,5 @@
 public class WhatToWear {
+    public static void main(String[] args) {
+
+    }
 }
